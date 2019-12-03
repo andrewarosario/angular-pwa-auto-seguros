98d6405f57fe180455065e741dc44e74e20c298b
Andrew Rosário <andrew.arosario@gmail.com>
2019-12-03 20:58:58 -0300

    adicionando roteamento para os componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CadastroSegurosComponent } from './components/cadastro-seguros/cadastro-seguros.component';
+import { ListarSegurosComponent } from './components/listar-seguros/listar-seguros.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'cadastro' },
+  { path: 'cadastro', component: CadastroSegurosComponent },
+  { path: 'listar', component: ListarSegurosComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
