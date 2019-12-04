38d8092c421b04b1b492c832c8628beb9098231a
Andrew Rosário <andrew.arosario@gmail.com>
2019-12-04 20:44:35 -0300

    importando módulos de http

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,9 @@ import { SuiModule } from 'ng2-semantic-ui';
 import { MenuComponent } from './components/menu/menu.component';
 import { CadastroSegurosComponent } from './components/cadastro-seguros/cadastro-seguros.component';
 import { ListarSegurosComponent } from './components/listar-seguros/listar-seguros.component';
+import { FormsModule } from '@angular/forms';
+import {HTTP_INTERCEPTORS, HttpClientJsonpModule, HttpClientModule} from '@angular/common/http';
+import { HttpInterceptorService } from './shared/services/http-interceptor.service';
 
 @NgModule({
   declarations: [
@@ -21,10 +24,19 @@ import { ListarSegurosComponent } from './components/listar-seguros/listar-segur
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    HttpClientJsonpModule,
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
     SuiModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: HttpInterceptorService,
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
