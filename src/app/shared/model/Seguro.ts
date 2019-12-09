75d6a279f17ca85ae833bee8a79c0b9221db4a5c
Andrew Rosário <andrew.arosario@gmail.com>
2019-12-09 20:12:21 -0300

    atualizando modelos da aplicação

--- src/app/shared/model/Seguro.ts
+++ src/app/shared/model/Seguro.ts
@@ -1,20 +1,13 @@
 import { MarcaCarro } from './MarcaCarro';
 import { Proprietario } from './Proprietario';
-import { Foto } from './Foto';
 
 export class Seguro {
   marcaCarro: MarcaCarro;
   modeloCarro: string;
   placaCarro: string;
   proprietario: Proprietario;
-  fotos: Foto[];
 
   constructor() {
     this.proprietario = {} as Proprietario;
-    this.fotos = [];
-  }
-
-  public get totalFotos() {
-    return this.fotos.length;
   }
 }
