2c7b95b48b4520550465a75f533e98ab2386d64a
Andrew Rosário <andrew.arosario@gmail.com>
2019-12-10 22:04:56 -0300

    verificando quando há nova versão disponível

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-pwa-auto-seguros';
+
+  constructor(private swUpdate: SwUpdate) {}
+
+  ngOnInit() {
+    if (this.swUpdate.isEnabled) {
+      this.swUpdate.available.subscribe(() => {
+        if (confirm('Nova versão disponível. Deseja recarregar a página?')) {
+          window.location.reload();
+        }
+      });
+    }
+  }
 }
