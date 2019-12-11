c5cc7972be89f8cc80bd498d1d6cc71b63cd5f99
Andrew Rosário <andrew.arosario@gmail.com>
2019-12-10 22:04:26 -0300

    incluindo versao da aplicação

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
+  version: '1.0',
   API_CARROS: 'https://www.carqueryapi.com/api/0.3/?callback=?&',
   API: 'http://localhost:9000',
   VAPID_PUBLIC_KEY: 'BBl5Vw0PCEM8nbonAjahMaBPAR3MEibrU-zwkXHd0vp_bL4w43ej_K41pLBWOIjCW_3TnotZvskdY_Xmg0Hde3I'
